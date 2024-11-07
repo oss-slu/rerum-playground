b79a77f081fca5175095ce8aae51d624730f9436
alar12 <alar12@users.noreply.github.com>
2024-11-07 08:36:34 +0000

    deploy: eeea8bd37ed50fddaa84d12ff572566ab15b1a59

--- js/playground.js
+++ js/playground.js
@@ -227,3 +227,31 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error("Error initializing the playground: ", err);
     }
 });    
+
+//fetch footer
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading footer:', error));
+
+//fetch menu
+fetch('menu.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('menu-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading menu:', error));
+
+//menubar js 
+function openCloseMenu() {
+    var toolBar = document.getElementById("toolBar");
+    var mainContent = document.querySelector(".content") || document.querySelector(".container") || document.querySelector("#tool_set");
+    toolBar.classList.toggle("sidebar-open");
+    if (mainContent) {
+        mainContent.classList.toggle("shift");
+    }
+}
+
+window.openCloseMenu = openCloseMenu;
