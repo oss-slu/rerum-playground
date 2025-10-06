a174c0563ff001671fb89a4576f3c8526c5b76d9
LuisPalmejar21 <157167413+LuisPalmejar21@users.noreply.github.com>
2025-10-06 10:50:59 -0500

    Revert "Changes for Issue #62 "

--- web/js/json-utils.test.js
+++
@@ -1,9 +0,0 @@
-const validateJSON = require('./json-utils.js');
-
-test('Testing prettifying JSON', () => {
-  expect(prettifyJSON("Testing")).toBe(true);
-});
-
-test('Testing validating JSON', () => {
-  expect(validateJSON("Testing")).toBe(false);
-});
